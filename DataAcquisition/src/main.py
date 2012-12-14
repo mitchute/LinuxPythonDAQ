97be5efaaacf555af9655d31e78cc99f0a9dc0f3
Edwin <edwin@mae-df-303-u-b.(none)>
2012-12-14 14:55:36 -0600

    Clean up and commenting Change baudrate so it works nicely, add smaller timeout Error handle when no readings are found Improve output a bit

--- DataAcquisition/src/main.py
+++ DataAcquisition/src/main.py
@@ -4,23 +4,44 @@ import time
 import serial
 from datetime import datetime 
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(port='/dev/ttyUSB0',baudrate=9600)
+# configure the serial connections 
+ser = serial.Serial(port='/dev/ttyUSB0', baudrate=2400, timeout=0.1)
 
+# open the serial connection
 ser.open()
 
-channels = 8
-num = 48 + channels 
-while True:
-	print datetime.now()
-	ser.write('!0RA' + chr(num))
-	x = channels
-	while x >= 0:
+# initialize a constant for convenience
+iZeroChar = ord('0') # should be 48, but this looks a bit nicer
+print iZeroChar
+
+# configure channels here, and transmit character
+numChannels = 4
+maxChannel = numChannels - 1 # zero-based
+cMaxChannel = chr(iZeroChar + maxChannel)
+
+# infinite loop while we read and spew data
+while True:	
+	# send a transmit signal
+	ser.write('!0RA' + cMaxChannel)
+	# re-initialize the values list
+	values = []
+	# loop over all channels
+	for x in range(numChannels):
+		# read msb and lsb bytes
 		msb = ser.read(1)
 		lsb = ser.read(1)
-		read = (ord(msb)*256) + ord(lsb)
-		print x, read	
-		x = x - 1	
-	time.sleep(1)
+		# check for errors
+		if msb == '' or lsb == '':
+			print "Could not receive data"
+			values.append(-9999)
+		else:
+			# calculate a reading value
+			read = (ord(msb)*256) + ord(lsb)
+			# store it in the list
+			values.append(read)
+	# spew
+	print "%s" % datetime.now(), values
+	# pause
+	time.sleep(0.25)
 	
 	
